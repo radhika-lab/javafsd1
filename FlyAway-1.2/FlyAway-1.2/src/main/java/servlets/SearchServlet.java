84fac078e29e90679e6c3dfd2c6551cf6c274188
Radhika <radhikaghanekar82@gmail.com>
2021-10-05 14:31:55 +0530

    flyaway project

---
+++ FlyAway-1.2/FlyAway-1.2/src/main/java/servlets/SearchServlet.java
@@ -0,0 +1,28 @@
+package servlets;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import jakarta.servlet.http.*;
+import model.Search;
+
+
+
+public class SearchServlet extends HttpServlet {
+
+    @Override
+    public void service (HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Search.date = req.getParameter("date");
+        Search.source = req.getParameter("source");
+        Search.destination = req.getParameter("destination");
+        Search.persons = Integer.parseInt(req.getParameter("persons"));
+        Search.day = getDay(Search.date);
+
+        resp.sendRedirect("/search-results.jsp");
+    }
+
+    public String getDay(String dateInp) {
+        LocalDate dt = LocalDate.parse(dateInp);
+        return dt.getDayOfWeek().toString();
+    }
+}
